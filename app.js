acc69bce0f36752e257b228a83123f989ab7add4
SeunPil <cushion2@naver.com>
2022-06-07 02:10:14 +0900

    Project END

--- app.js
+++ app.js
@@ -6,11 +6,12 @@ const list = document.querySelectorAll('.textlist');
 window.addEventListener("scroll", (e) => {
     // ViewPort의 Y값
     const scrollY = this.scrollY;
+    console.log(scrollY);
 
 
     // 이벤트를 실행시킬 Y좌표값
     // 노트북 1896
-    if (scrollY == 1896) {
+    if (scrollY == 1848) {
         for (var i = 0; i < 4; i++) {
             const graph = document.getElementsByClassName('graph')[i];
             graph.id = 'graph' + i
@@ -22,7 +23,7 @@ window.addEventListener("scroll", (e) => {
         }
     }
     // 노트북 952
-    if (scrollY == 952) {
+    if (scrollY == 928) {
         // 아이디 변경
         // for문을 반복해 아이디를 변경하고 i값을 이용한 애니메이션 실행
         for (var i = 0; i < 4; i++) {
